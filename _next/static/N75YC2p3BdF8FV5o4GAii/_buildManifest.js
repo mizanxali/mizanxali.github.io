ee9d95113f0db4401ef9edce45c3d1d9f2b70241
Mizanali Panjwani <mizanalip786@gmail.com>
2022-04-08 05:34:40 +0530

    Updates

---
+++ _next/static/N75YC2p3BdF8FV5o4GAii/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F1bfc9850.2d38f5de38fb7e5c5ede.js","static\u002Fchunks\u002Fpages\u002Findex-ce68d42ee2f0c1becbfd.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-3afb1b22b5ac1967ee73.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
